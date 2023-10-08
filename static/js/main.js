a1ace260f79551c23ccc9988c1dafdca2ddd1390
Cherif hassan Nousradine <cherif-hassan.nousradine@loria.fr>
2023-10-08 23:21:07 +0200

    added a pdf reader

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+const pdfUrl = "static/Git_course_IDMC_UL.pdf"
 
 function toggleCollapsibleParagraphs() {
     var paragraphs = document.getElementById("collapsible-paragraphs");
@@ -10,4 +11,32 @@ function toggleCollapsibleParagraphs() {
         button.innerHTML = "Read more";
     }
 }
+
+// let pdfjsLib = window['static/js/pdfjs/build/pdf'];
+const container = document.getElementById('pdfViewer');
+
+pdfjsLib.getDocument(pdfUrl).promise.then(function(pdf){
+    for(let pageNumber=1; pageNumber<=pdf.numPages; pageNumber){
+
+        pdf.getPage(pageNumber).then(function(page){
+            const canvas = document.createElement('canvas');
+            container.appendChild(canvas);
+            const viewport = page.getViewport({scale:1.5});
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+            const context = canvas.getContext('2d');
+
+            page.render({
+                canvasContext:context,
+                viewport: viewport
+            });
+
+        });
+
+    }
+}).catch(function(error){console.error("Error loading Pdf", error)});
+
+
+
+
 
